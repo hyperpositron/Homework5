b40af2ab27f31834c77f55341f6b4f5af0193727
Александр <hyperpositron@gmail.com>
2023-01-05 15:46:17 +0200

    first commit gh

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+    public static void task1 () {
+        //Задача 1
+        System.out.println("Задача №1");
+
+
     }
 }
