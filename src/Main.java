51496072739b7340349e1ae496dc129dc45959b0
Александр <hyperpositron@gmail.com>
2023-01-05 23:10:00 +0200

    task 2

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
-        task3();
-        task4();
-        task5();
+        //task3();
+        //task4();
+        //task5();
     }
     public static void task1 () {
         //Задача 1
@@ -16,4 +16,26 @@ public class Main {
             System.out.println("«Установите версию приложения для iOS по ссылке»");
         }
     }
+    public static void task2 () {
+        //Задача 2
+        System.out.println("Задача №2");// 1 - android, 0 - iOS
+        int clientDeviceYear = 2015;
+        int clientOS = 1;
+
+        if (clientDeviceYear >= 2015) {
+            if (clientOS == 1) {
+                System.out.println("«Установите версию приложения для Android по ссылке»");
+            } else {
+                System.out.println("«Установите версию приложения для iOS по ссылке»");
+            }
+        } else {
+            if (clientDeviceYear < 2015)
+             if  (clientOS == 1) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+        }
+    }
+
 }
