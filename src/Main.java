6912296bc95c00a943c512af45ad13bcb97cffa7
Александр <hyperpositron@gmail.com>
2023-01-05 16:18:05 +0200

    task 1

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
+        task3();
+        task4();
+        task5();
     }
     public static void task1 () {
         //Задача 1
         System.out.println("Задача №1");
-
-
+        int clientOS = 1;
+        if (clientOS == 1) {
+            System.out.println("«Установите версию приложения для Android по ссылке»");
+        } else {
+            System.out.println("«Установите версию приложения для iOS по ссылке»");
+        }
     }
 }
