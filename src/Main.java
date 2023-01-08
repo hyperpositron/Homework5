43af3e896dd87b74910769ae42ce10e68dc3f896
Александр <hyperpositron@gmail.com>
2023-01-08 15:43:24 +0200

    task5

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         task2_3();
         task3();
         task4();
-        //task5();
+        task5();
     }
 
     public static void task1() {
@@ -67,29 +67,79 @@ public class Main {
         String version = clientDeviceYear >= 2015 ? "" : "облегченную";
         System.out.println("Установите " + version + " версию приложения для " + osString + " по ссылке");
     }
+
     public static void task3() {
         //Задача 3 Определим, является ли год високосным или нет.
         System.out.println("Задача №3 Определим, является ли год високосным или нет.");
         int year = 2023;                                                  /*  (если)   (год деленый на 4, остаток должен быть = 0)                    */
         if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {    /*   (и)     (год деленый на 100, остаток должен быть != 0)                 */
-            System.out.println( + year + " год является високосным. ");   /*   (или)   (год деленый на 400, остаток должен быть = 0)                  */
+            System.out.println(+year + " год является високосным. ");   /*   (или)   (год деленый на 400, остаток должен быть = 0)                  */
         } else {                                                          /* Небольшая справка: високосным является каждый 4-ый год,                  */
-            System.out.println( + year + " год не является високосным."); /* но не является каждый 100ый. Также високосным является каждый 400ый год. */
+            System.out.println(+year + " год не является високосным."); /* но не является каждый 100ый. Также високосным является каждый 400ый год. */
         }
 
     }
+
     public static void task4() {
         //Задача 4
         System.out.println("Задача №4");
         int deliveryDistance = 70; // дистанция до клиента
-        if (deliveryDistance <=20){
+        if (deliveryDistance <= 20) {
             System.out.println("Потребуется 1 день");
-        } else if (deliveryDistance <=60){
+        } else if (deliveryDistance <= 60) {
             System.out.println("Потребуется 2 дня");
-        } else if (deliveryDistance <=100){
+        } else if (deliveryDistance <= 100) {
             System.out.println("Потребуется 3 дня");
-        } else if (deliveryDistance > 100){
+        } else if (deliveryDistance > 100) {
             System.out.println("Доставки нет");
+        }
+    }
+
+    public static void task5() {
+        //Задача 5 - определяет по номеру месяца в году, к какому сезону этот месяц принадлежит
+        System.out.println("Задача №5");
+        int monthNumber = 13;
+        String monthString;
+        switch (monthNumber) {
+            case 1:  monthString = "Январь";
+                System.out.println(monthString + " принадлежит к сезону зима.");
+                break;
+            case 2:  monthString = "Февраль";
+                System.out.println(monthString + " принадлежит к сезону зима.");
+                break;
+            case 3:  monthString = "Март";
+                System.out.println(monthString + " принадлежит к сезону весны.");
+                break;
+            case 4:  monthString = "Апрель";
+                System.out.println(monthString + " принадлежит к сезону весны.");
+                break;
+            case 5:  monthString = "Май";
+                System.out.println(monthString + " принадлежит к сезону весны.");
+                break;
+            case 6:  monthString = "Июнь";
+                System.out.println(monthString + " принадлежит к сезону лето.");
+                break;
+            case 7:  monthString = "Июль";
+                System.out.println(monthString + " принадлежит к сезону лето.");
+                break;
+            case 8:  monthString = "Август";
+                System.out.println(monthString + " принадлежит к сезону лето.");
+                break;
+            case 9:  monthString = "Сентябрь";
+                System.out.println(monthString + " принадлежит к сезону осень.");
+                break;
+            case 10: monthString = "Октябрь";
+                System.out.println(monthString + " принадлежит к сезону осень.");
+                break;
+            case 11: monthString = "Ноябрь";
+                System.out.println(monthString + " принадлежит к сезону осень.");
+                break;
+            case 12: monthString = "Декабрь";
+                System.out.println(monthString + " принадлежит к сезону зима.");
+                break;
+            default: monthString = "Не знаем такого";
+                System.out.println(monthString);
+                break;
         }
     }
 }
